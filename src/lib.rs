3f7dd0de0430a931a1f9cea31c5e5e30f0fa437b
Alexander Sukharev <sukharev@prognoz.ru>
2015-11-06 18:55:17 +0300

    Add generic

--- src/lib.rs
+++ src/lib.rs
@@ -3,26 +3,35 @@ use position::Position;
 mod size;
 use size::Size;
 
-#[derive(Debug)]
-pub struct M {
+#[derive(Debug, Default)]
+pub struct M<T> {
     rows: usize,
     columns: usize,
-    values: Vec<u16>
+    values: Vec<T>
 }
 
-pub trait Convert {
-    fn into_matrix(self, s: &Size) -> M;
+trait MyClone {
+    fn clone(&self) -> Self;
 }
-impl Convert for Vec<u16>{
-    fn into_matrix(self, s: &Size) -> M {
-        let mut v: Vec<u16> = Vec::new();
+
+impl MyClone for u16 {
+    fn clone(&self) -> Self { *self }
+}
+
+pub trait Convert<T> {
+    fn into_matrix(&self, s: &Size) -> M<T>;
+}
+
+impl<T> Convert<T> for Vec<T> where T: Default + MyClone {
+    fn into_matrix(&self, s: &Size) -> M<T> {
+        let mut v: Vec<T> = Vec::new();
         let count = s.rows() * s.columns();
         let mut it = self.iter();
         let mut idx = 0;
         while idx < count {
-            let value: u16 = match it.next() {
+            let value: T = match it.next() {
                 Some(v) => v.clone(),
-                None => 0
+                None => Default::default()
             };
             v.push(value);
             idx += 1;
@@ -31,34 +40,35 @@ impl Convert for Vec<u16>{
     }
 }
 
-pub trait Matrix {
-    fn set(&mut self, p: &Position, v: u16);
-    fn get(&self, p: &Position) -> u16;
-    fn sum(&self, m: &M) -> M;
-    fn transpose(&self) -> M;
+pub trait Matrix<T> {
+    fn set(&mut self, p: &Position, v: T);
+    fn get(&self, p: &Position) -> T;
+    // fn sum(&self, m: &M) -> M;
+    fn transpose(&self) -> M<T>;
 }
 
-impl Matrix for M {
-    fn set(&mut self, p: &Position, v: u16) {
-        self.values[p.column() + (p.row() * self.columns)] = v;
+impl<T> Matrix<T> for M<T> where T: Default + MyClone {
+    fn set(&mut self, p: &Position, v: T) {
+        self.values[p.column() + (p.row() * self.columns)] = v.clone();
     }
-    fn get(&self, p: &Position) -> u16 {
-        self.values[p.column() + (p.row() * self.columns)]
+    fn get(&self, p: &Position) -> T {
+        let v = &self.values[p.column() + (p.row() * self.columns)];
+        v.clone()
     }
-    fn sum(&self, m: &M) -> M {
-        assert!((self.rows == m.rows) & (self.columns == m.columns));
-        let zv = self.values.iter().zip(&m.values);
-        let v: Vec<u16> = zv.map(|(a, b)| {
-            a + b
-        }).collect();
-        M { rows: self.rows, columns: self.columns, values: v}
-    }
-    fn transpose(&self) -> M {
-        let mut v: Vec<u16> = Vec::new();
+    // fn sum(&self, m: &M) -> M {
+    //     assert!((self.rows == m.rows) & (self.columns == m.columns));
+    //     let zv = self.values.iter().zip(&m.values);
+    //     let v: Vec<u16> = zv.map(|(a, b)| {
+    //         a + b
+    //     }).collect();
+    //     M { rows: self.rows, columns: self.columns, values: v}
+    // }
+    fn transpose(&self) -> M<T> {
+        let mut v: Vec<T> = Vec::new();
         for column in 0..self.columns {
             for row in 0..self.rows {
                 let value = self.get(&(row, column));
-                v.push(value);
+                v.push(value.clone());
             }
         }
         v.into_matrix(&(self.columns, self.rows))
@@ -67,17 +77,18 @@ impl Matrix for M {
 
 #[test]
 fn it_works() {
-    let mut m: M = vec![1,2,3].into_matrix(&(3,3));
+    let v: Vec<u16> = vec![1,2,3];
+    let mut m = v.into_matrix(&(3,3));
     assert_eq!(m.values, vec![1, 2, 3, 0, 0, 0, 0, 0, 0]);
 
-    let p = (1,1);
-    m.set(&p, 2);
+    let p = &(1,1);
+    m.set(p, 2);
     assert_eq!(m.values, vec![1, 2, 3, 0, 2, 0, 0, 0, 0]);
-    let gv = m.get(&p);
+    let gv = m.get(p);
     assert_eq!(gv, 2);
 
-    let mt: M = m.transpose();
+    let mt: M<u16> = m.transpose();
     assert_eq!(mt.values, vec![1, 0, 0, 2, 2, 0, 3, 0, 0]);
 
-    assert_eq!(m.sum(&mt).values, vec![2, 2, 3, 2, 4, 0, 3, 0, 0]);
+    // assert_eq!(m.sum(&mt).values, vec![2, 2, 3, 2, 4, 0, 3, 0, 0]);
 }
