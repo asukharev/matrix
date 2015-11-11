d84a7f10168721858283d815d0764c730a792f2b
Alexander Sukharev <sukharev@prognoz.ru>
2015-11-11 13:32:29 +0300

    mult impl for types

--- src/math.rs
+++ src/math.rs
@@ -49,36 +49,43 @@ add_impl! { usize u8 u16 u32 u64 isize i8 i16 i32 i64 f32 f64 }
 //-------------------------------------------------------------------------------------------------
 use std::ops::Mul;
 
-impl Mul for M<u16> {
-    type Output = Result<M<u16>, String>;
-    fn mul(self, m: M<u16>) -> Result<M<u16>, String> {
-        if self.columns != m.rows {
-            Err("Some error message3".to_string())
-        }
-        else {
-            let mut mr: Vec<u16> = Vec::new();
-            for va in self.values.chunks(self.columns) {
-                let mut m_row: Vec<u16> = Vec::new();
-                for column in 0..va.len() {
-                    let vb: Vec<_> = m.values.iter().enumerate().filter_map(|(idx, &x)|
-                        if idx % m.rows == column { Some(x) } else { None }
-                    ).collect();
-                    let r: Vec<u16> = va.iter().zip(&vb)
-                        .map(|(a,b)| {
-                            a * b
-                        })
-                        .collect();
-                    let sum = r.iter().fold(0, |acc, &item| acc + item);
-                    m_row.push(sum);
+macro_rules! mul_impl {
+    ($($t:ty)*) => ($(
+        impl Mul for M<$t> {
+            type Output = Result<M<$t>, String>;
+            #[inline]
+            fn mul(self, m: M<$t>) -> Result<M<$t>, String> {
+                if self.columns != m.rows {
+                    Err("Some error message3".to_string())
                 }
-                for v in m_row {
-                    mr.push(v);
+                else {
+                    let mut mr: Vec<$t> = Vec::new();
+                    for va in self.values.chunks(self.columns) {
+                        let mut m_row: Vec<$t> = Vec::new();
+                        for column in 0..va.len() {
+                            let vb: Vec<$t> = m.values.iter().enumerate().filter_map(|(idx, &x)|
+                                if idx % m.rows == column { Some(x) } else { None }
+                            ).collect();
+                            let r: Vec<$t> = va.iter().zip(&vb)
+                                .map(|(a,b)| {
+                                    a * b
+                                })
+                                .collect();
+                            let sum = r.iter().fold(0 as $t, |acc, &item| acc + item);
+                            m_row.push(sum);
+                        }
+                        for v in m_row {
+                            mr.push(v);
+                        }
+                    }
+                    Ok(M { rows: self.rows, columns: self.columns, values: mr})
                 }
             }
-            Ok(M { rows: self.rows, columns: self.columns, values: mr})
         }
-    }
+    )*)
 }
+
+mul_impl! { usize u8 u16 u32 u64 isize i8 i16 i32 i64 f32 f64 }
 
 // #[test]
 // fn t() {
