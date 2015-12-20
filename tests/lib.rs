094575aa6c8d8ebf4a3f32634af1d4edf6ab5f1b
Alexander Sukharev <asuk1979@gmail.com>
2015-12-20 04:01:46 +0300

    Cleanup, add test for strings and iterator

--- tests/lib.rs
+++ tests/lib.rs
@@ -3,9 +3,16 @@ use matrix::Matrix;
 
 #[test]
 fn check_new() {
-    let v: Vec<i32> = vec![1, 2, 3];
-    let m = Matrix::from((3, 3, v));
-    assert_eq!(m.values, vec![1, 0, 0, 2, 0, 0, 3, 0, 0]);
+    {
+        let v: Vec<i32> = vec![1, 2, 3];
+        let m = Matrix::from((3, 3, v));
+        assert_eq!(m.values, vec![1, 0, 0, 2, 0, 0, 3, 0, 0]);
+    }
+    {
+        let vs: Vec<String> = vec!["A".to_string(), "B".to_string(), "C".to_string()];
+        let ms = Matrix::from((3, 3, vs));
+        assert_eq!(ms.values, vec!["A", "", "", "B", "", "", "C", "", ""]);
+    }
 }
 
 #[test]
@@ -37,14 +44,20 @@ fn check_get_value() {
 //
 #[test]
 fn check_transpose() {
-    let v: Vec<i32> = vec![1, 2, 3, 0, 2, 0, 0, 2, 0];
-    let m = Matrix::from((3, 3, v));
-
-    let mt: Matrix<i32> = m.transpose();
-    assert_eq!(mt.values, vec![1, 2, 3, 0, 2, 0, 0, 2, 0]);
-
-    let mtt: Matrix<i32> = mt.transpose();
-    assert_eq!(m.values, mtt.values);
+    {
+        let v: Vec<i32> = vec![1, 2, 3, 0, 2, 0, 0, 2, 0];
+        let m = Matrix::from((3, 3, v));
+        let mt: Matrix<i32> = m.transpose();
+        assert_eq!(mt.values, vec![1, 2, 3, 0, 2, 0, 0, 2, 0]);
+        let mtt: Matrix<i32> = mt.transpose();
+        assert_eq!(m.values, mtt.values);
+    }
+    {
+        let v: Vec<String> = vec!["A".to_string(), "B".to_string(), "C".to_string()];
+        let m = Matrix::from((3, 3, v));
+        let mt: Matrix<String> = m.transpose();
+        assert_eq!(mt.values, vec!["A", "B", "C", "", "", "", "", "", ""]);
+    }
 }
 
 //
@@ -131,15 +144,29 @@ fn check_multiplication_by_vector_err() {
 //
 #[test]
 fn check_sum() {
-    let v: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let m = Matrix::from((3, 3, v));
-    let mt: Matrix<i32> = m.transpose();
+    {
+        let v: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let m = Matrix::from((3, 3, v));
+        let mt: Matrix<i32> = m.transpose();
+        match m.clone() + mt.clone() {
+            Ok(r) => {
+                assert_eq!(r.values, vec![2, 6, 10, 6, 10, 14, 10, 14, 18]);
+            },
+            Err(why) => println!("{:?}", why),
+        }
+    }
 
-    match m.clone() + mt.clone() {
-        Ok(r) => {
-            assert_eq!(r.values, vec![2, 6, 10, 6, 10, 14, 10, 14, 18]);
-        },
-        Err(why) => println!("{:?}", why),
+    {
+        let v: Vec<String> = vec!["A".to_string(), "B".to_string(), "C".to_string()];
+        let m = Matrix::from((3, 3, v));
+        let mt: Matrix<String> = m.transpose();
+        match m + mt {
+            Ok(mmt) => {
+                println!("{:?}", mmt);
+                assert_eq!(mmt.values, vec!["AA", "B", "C", "B", "", "", "C", "", ""]);
+            },
+            Err(why) => println!("{:?}", why),
+        }
     }
 }
 
@@ -160,22 +187,13 @@ fn check_sum_err() {
     assert_eq!(r, "Dissimilar multidimensional matrix");
 }
 
-//-------------------------------------------------------------------------------------------------
-// #[test]
-// fn it_works() {
-//     { // Strings
-//         let v: Vec<String> = vec!["A".to_string(), "B".to_string(), "C".to_string()];
-//         // let m: M<String> = v.into_matrix(&(3,3));
-//         let m = Matrix::from((3, 3, v));
-//         assert_eq!(m.values, vec!["A", "B", "C", "", "", "", "", "", ""]);
-//         let mt: Matrix<String> = m.transpose();
-//         assert_eq!(mt.values, vec!["A", "", "", "B", "", "", "C", "", ""]);
-//         match m + mt {
-//             Ok(mmt) => {
-//                 println!("{:?}", mmt);
-//                 assert_eq!(mmt.values, vec!["AA", "B", "C", "B", "", "", "C", "", ""]);
-//             },
-//             Err(why) => println!("{:?}", why),
-//         }
-//     }
-// }
+#[test]
+fn check_iter() {
+    let v: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let m = Matrix::from((3, 3, v));
+    for mv in m.iter() {
+        let (v, (row, column)) = mv;
+        let gv = m.get(row, column);
+        assert_eq!(v, gv);
+    }
+}
